8f8bf6b14bba75daa1bb28b09d0abe20cada2059
Cintya Olivares <ext_cintya.olivares@cajalosandes.cl>
2023-07-13 09:00:18 -0400

    desafio1 JS G41

---
+++ assets/JS/DesafioUno.js
@@ -0,0 +1,19 @@
+const precio = 400000
+
+function calcularValor() {
+    let numero = document.querySelector("#cantidad").value;
+    let resultado = numero * precio;
+    document.querySelector("#total").textContent = resultado;
+
+
+    let cantidadFinall= document.querySelector('#cantidad').value;
+    document.querySelector(' #cantidadFinal').textContent = cantidadFinall;
+
+    let colorFinal= document.querySelector('#color').value;  
+    document.querySelector('#colorMuestra').style.backgroundColor = colorFinal;
+
+}
+
+
+
+
